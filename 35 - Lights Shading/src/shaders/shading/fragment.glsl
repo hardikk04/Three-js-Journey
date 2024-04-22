b320821ff3d81059a267f7d9e9bcf661cf7043c1
hardikk04 <hardik04062003@gmail.com>
2024-04-22 17:50:01 +0530

    35 - Lights Shading added

---
+++ 35 - Lights Shading/src/shaders/shading/fragment.glsl
@@ -0,0 +1,68 @@
+uniform vec3 uColor;
+
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+
+#include ../includes/ambientLight.glsl;
+#include ../includes/directionalLight.glsl;
+#include ../includes/pointLight.glsl;
+
+
+
+
+
+void main()
+{
+    vec3 normal = normalize(vNormal);
+
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+
+    vec3 color = uColor;
+
+    vec3 light = vec3(0.0);
+      light += ambientLight(
+        vec3(1.0,0.0,0.0),    // Light color
+        0.03                  // Light intensity
+    );
+
+    light += directionalLight(
+        vec3(0.1,0.1,1.0),     // Light color
+        1.0,                   // Light Intensity
+        normal,                // Noraml
+        vec3(0.0,0.0,3.0),     // Light position
+        viewDirection,         // View direction
+        20.0                   // Specular power
+    );
+
+    light += pointLight(
+         vec3(1.0,0.1,0.1),       // Light color
+         1.0,                     // Light Intensity
+         normal,                  // Noraml
+         vec3(0.0,2.5,0.0),       // Light position
+         viewDirection,           // View direction
+         20.0,                    // Specular power
+         vPosition,               // Position
+         0.25                     // Position
+     );
+
+    light += pointLight(
+         vec3(1.0,0.1,0.1),       // Light color
+         1.0,                     // Light Intensity
+         normal,                  // Noraml
+         vec3(0.0,2.5,0.0),       // Light position
+         viewDirection,           // View direction
+         20.0,                    // Specular power
+         vPosition,               // Position
+         0.22                     // Position
+     );
+
+    color *= light;
+
+    
+
+    // Final color
+    gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
